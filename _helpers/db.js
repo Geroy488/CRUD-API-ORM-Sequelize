df428828ed27c0ebb6a72afec5dde505d7a64911
Geroy488 <mayecha302@gmail.com>
2025-04-04 14:43:39 +0800

    Adding Updates

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,4 +1,4 @@
-const  config = require('config.json');
+const config = require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
@@ -9,7 +9,7 @@ initialize();
 async function initialize() {
   // create db if it doesn't already exist
   const { host, port, user, password, database } = config.database;
-
+  
   const connection = await mysql.createConnection({ host, port, user, password });
   await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
